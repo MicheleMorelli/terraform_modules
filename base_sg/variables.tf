12e1276d9885dfad39511bde4c778b95e7bc901b
MicheleMorelli <morelli.michele@hotmail.it>
2020-06-06 16:51:00 +0100

    added list vars

--- base_sg/variables.tf
+++ base_sg/variables.tf
@@ -2,3 +2,13 @@ variable "name" {
   type        = string
   description = "Name prefix of the SG"
 }
+
+variable "port_list" {
+  type        = list(string)
+  description = "List of ports to create rules for"
+}
+
+variable "cidr_list" {
+  type        = list(string)
+  description = "List of CIDRs"
+}
