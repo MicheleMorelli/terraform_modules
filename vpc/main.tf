0768b149cdf01bf7817c6bd54652e5b817eaf1ac
MicheleMorelli <morelli.michele@hotmail.it>
2020-06-07 20:57:44 +0100

    linting and fixed issue

--- vpc/main.tf
+++ vpc/main.tf
@@ -9,8 +9,8 @@ resource "aws_vpc" "this" {
 }
 
 resource "aws_internet_gateway" "this" {
-  vpc_id = $aws_vpc.this.id
-  
+  vpc_id = aws_vpc.this.id
+
   tags = {
     Name = "${var.name}-gw"
   }
--- vpc/variables.tf
+++ vpc/variables.tf
@@ -1,9 +1,9 @@
 variable "name" {
-  type = string
+  type        = string
   description = "The name of the resource"
 }
 
 variable "vpc_cidr_range" {
-  type = string
+  type        = string
   description = "the CIDR range of the VPC"
 }
