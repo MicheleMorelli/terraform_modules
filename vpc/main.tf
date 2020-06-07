606c22d41c27f23f96a267c7821dc206f5a6cbc7
MicheleMorelli <morelli.michele@hotmail.it>
2020-06-08 00:05:12 +0100

    fixed igw issue

--- vpc/main.tf
+++ vpc/main.tf
@@ -32,7 +32,7 @@ resource "aws_route_table" "public" {
 resource "aws_route" "public_route" {
   route_table_id         = aws_route_table.public.id
   destination_cidr_block = "0.0.0.0/0"
-  gateway_id             = aws_internet_gateway.this
+  gateway_id             = aws_internet_gateway.this.id
   depends_on             = [aws_route_table.public]
 }
 
