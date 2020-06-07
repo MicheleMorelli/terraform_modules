648c3714447acb414804d4477217ec78f69b5eb4
MicheleMorelli <morelli.michele@hotmail.it>
2020-06-07 21:46:40 +0100

    adding route tables

--- vpc/main.tf
+++ vpc/main.tf
@@ -16,10 +16,34 @@ resource "aws_internet_gateway" "this" {
   }
 }
 
+
+# From the TF doc:
+# Note that the default route, mapping the VPC's CIDR block to "local", is created implicitly and cannot be specified.
+# https://www.terraform.io/docs/providers/aws/r/default_route_table.html
 resource "aws_route_table" "public" {
   vpc_id = aws_vpc.this.id
 
   tags = {
     Name = "${var.name}-public-rt"
+  }
+}
+
+
+resource "aws_route" "public_route" {
+  route_table_id            = aws_route_table.public.id
+  destination_cidr_block    = "0.0.0.0/0"
+  gateway_id = aws_internet_gateway.this
+  depends_on                = [aws_route_table.public]
+  
+  tags = {
+    Name = "${var.name}-public-route"
+  }
+}
+
+resource "aws_default_route_table" "default" {
+  default_route_table_id = aws_vpc.this.default_route_table_id
+
+  tags = {
+    Name = "${var.name}-default-route-table"
   }
 }
--- vpc/variables.tf
+++ vpc/variables.tf
@@ -7,3 +7,8 @@ variable "vpc_cidr_range" {
   type        = string
   description = "the CIDR range of the VPC"
 }
+
+variable "public_route_cidr_block" {
+  type        = string
+  description = "the destination CIDR block got the public route"
+}
