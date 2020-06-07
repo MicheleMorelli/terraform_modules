2bb4b5a734bda6c50f33c009d5fff92ca83b76aa
MicheleMorelli <morelli.michele@hotmail.it>
2020-06-07 21:47:01 +0100

    linting

--- vpc/main.tf
+++ vpc/main.tf
@@ -30,11 +30,11 @@ resource "aws_route_table" "public" {
 
 
 resource "aws_route" "public_route" {
-  route_table_id            = aws_route_table.public.id
-  destination_cidr_block    = "0.0.0.0/0"
-  gateway_id = aws_internet_gateway.this
-  depends_on                = [aws_route_table.public]
-  
+  route_table_id         = aws_route_table.public.id
+  destination_cidr_block = "0.0.0.0/0"
+  gateway_id             = aws_internet_gateway.this
+  depends_on             = [aws_route_table.public]
+
   tags = {
     Name = "${var.name}-public-route"
   }
