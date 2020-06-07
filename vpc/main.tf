8fd5ad78f9965bb4b4f3a618c395c025e3b4516e
MicheleMorelli <morelli.michele@hotmail.it>
2020-06-07 22:51:12 +0100

    added private subnet

--- vpc/main.tf
+++ vpc/main.tf
@@ -49,12 +49,23 @@ resource "aws_default_route_table" "default-private" {
 }
 
 resource "aws_subnet" "public" {
-  count = length(var.public_subnets_cidr_blocks_list)
-  vpc_id     = aws_vpc.this.id
-  cidr_block = var.public_subnets_cidr_blocks_list[count.index]
-  availability_zone = data.aws_availability_zones.available.names[0]
+  count             = length(var.public_subnets_cidr_blocks_list)
+  vpc_id            = aws_vpc.this.id
+  cidr_block        = var.public_subnets_cidr_blocks_list[count.index]
+  availability_zone = count.index % 2 == 0 ? data.aws_availability_zones.available.names[0] : data.aws_availability_zones.available.names[1]
 
   tags = {
-    Name = "${var.name}-public-subnet-${var.public_subnets_cidr_blocks_list[count.index]}"
+    Name = "${var.name}-public-subnet-${count.index}"
+  }
+}
+
+resource "aws_subnet" "private" {
+  count             = length(var.private_subnets_cidr_blocks_list)
+  vpc_id            = aws_vpc.this.id
+  cidr_block        = var.private_subnets_cidr_blocks_list[count.index]
+  availability_zone = count.index % 2 == 0 ? data.aws_availability_zones.available.names[0] : data.aws_availability_zones.available.names[1]
+
+  tags = {
+    Name = "${var.name}-private-subnet-${count.index}"
   }
 }
